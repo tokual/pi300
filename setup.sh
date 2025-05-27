61a731714b332b04ce552cf802a22ebc31c3712b
tokual <130680885+tokual@users.noreply.github.com>
2025-05-27 22:51:08 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Directory of the repo
+REPO_DIR="/home/pi300/pi300"
+
+# Install Python dependencies
+pip3 install -r "$REPO_DIR/requirements.txt" --quiet
+
+# You can add other setup commands here if needed
